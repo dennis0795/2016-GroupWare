9d9c11b06b6311711399b0299bf733aec99e8b62
laboratorio <laboratorio@LAB09-05>
2016-10-15 16:05:38 -0600

    creacion de nuevo proyecto

---
+++ WebApplication1/Groupware.AdventureWorks.Model1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groupware.AdventureWorks.Model1
+{
+    public class Class1
+    {
+    }
+}
