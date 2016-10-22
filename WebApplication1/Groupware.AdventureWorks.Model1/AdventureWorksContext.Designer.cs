1e1bc9ea17a7ebaf69bebd8754b82b815845c0b3
laboratorio <laboratorio@LAB09-06>
2016-10-22 16:22:08 -0600

    22/10/2016

--- WebApplication1/Groupware.AdventureWorks.Model1/AdventureWorksContext.Designer.cs
+++ WebApplication1/Groupware.AdventureWorks.Model1/AdventureWorksContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Dennis Rivera\WebApplication1\Groupware.AdventureWorks.Model1\AdventureWorksContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\laboratorio\Source\Repos\2016-GroupWare\WebApplication1\Groupware.AdventureWorks.Model1\AdventureWorksContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ WebApplication1/Groupware.AdventureWorks.Model1/MyTables/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groupware.AdventureWorks.Model1
+{
+    public partial class Product
+    {
+        public double AntiquityInYears
+        {
+            get
+            {
+                var antiquity = 0.0;
+                DateTime? miFechaFinal = DateTime.Now;
+
+
+                if(DiscontinuedDate != null)
+                {
+                    miFechaFinal = DiscontinuedDate;
+                }
+                TimeSpan ts = (TimeSpan)(miFechaFinal - SellStartDate);
+                antiquity = ts.Days;
+
+                return antiquity;
+            }
+
+        }
+        public decimal? WeightKilos {
+            get
+            {
+                decimal? weight = Weight;
+                if (WeightUnitMeasureCode == "G")
+                {
+                    weight /= 1000;
+                    
+                    }
+                else if (WeightUnitMeasureCode == "LB")
+                {
+                    weight /= Convert.ToDecimal(2.2);
+                }
+
+                return (weight);
+
+            }
+                
+                
+    }
+}
